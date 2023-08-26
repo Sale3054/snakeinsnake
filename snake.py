67178d306f4fa91b756f55e3a0bdfb224c478f07
Sleon <samuel.andrew.leon@gmail.com>
2023-08-25 19:17:31 -0600

    added assets, add base boilerplate for tinkering

--- snake.py
+++ snake.py
@@ -1,22 +1,52 @@
+#!/bin/python3
+# Example file showing a circle moving on screen
 import pygame
 
+# pygame setup
+pygame.init()
 
-if __name__ == "__main__":
+s_height = 1280
+s_width = 720
 
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
-    clock = pygame.time.Clock()
-    running = True
+screen = pygame.display.set_mode((s_height, s_width))
+clock = pygame.time.Clock()
+running = True
+dt = 0
 
-    while running:
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-        screen.fill("purple")
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+    snakeDim = (50, 100)
 
-        pygame.display.flip()
+    snakeRect = pygame.Rect(player_pos, snakeDim)
+    pygame.draw.rect(screen, "red", snakeRect)
 
-        clock.tick(60)
-    pygame.quit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+    if keys[pygame.K_q]:
+        pygame.quit()
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
